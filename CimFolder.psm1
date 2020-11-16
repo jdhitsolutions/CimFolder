1fd06d5d74f8b6fe4c478973b984f58cc2adc230
jdhitsolutions <jhicks@jdhitsolutions.com>
2020-11-16 16:58:59 -0500

    Initial commit

---
+++ CimFolder.psm1
@@ -0,0 +1,11 @@
+#requires -version 7.1
+
+#region Main
+
+Get-VMHardDiskDrive Set-VMHardDiskDrive Remove-VMHardDiskDrive Add-VMHardDiskDrive Enter-PSSession Invoke-PSSession
+
+#endregion
+
+#define aliases
+
+
---
+++ README.md
@@ -0,0 +1,6 @@
+# CimFolder
+
+{INSERT CONTENT}
+
+_last updated 11/16/2020 16:58:59_
+

a140f6eac952b19034e2106a96b8083be850f9ab
jdhitsolutions <jhicks@jdhitsolutions.com>
2020-11-16 17:22:14 -0500

    root module update

--- CimFolder.psm1
+++ CimFolder.psm1
@@ -1,7 +1,9 @@
 
+#dot source function files
 . $PSScriptRoot\functions\private.ps1
 . $PSScriptRoot\functions\public.ps1
 
+#add type extensions
 Update-TypeData -TypeName cimFolder -DefaultDisplayPropertySet Mode, LastModified, Size, Name -Force
 Update-TypeData -TypeName cimFile -MemberType AliasProperty -MemberName Size -Value FileSize -Force
 Update-TypeData -TypeName cimFile -DefaultDisplayPropertySet Mode, LastModified, Size, Name -Force
